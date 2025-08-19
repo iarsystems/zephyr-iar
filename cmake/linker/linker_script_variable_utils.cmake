bb084dea07964c491eb73ca902915edc158795e4
Björn Bergman <bjorn.bergman@iar.com>
2025-08-19 12:40:07 +0200

    Change the syntax to @foo:undef=u:other_var=blaha@

--- cmake/linker/linker_script_variable_utils.cmake
+++ cmake/linker/linker_script_variable_utils.cmake
@@ -13,17 +13,16 @@
 # the size of kobject from gen_kobject.py to be used when defining sections
 #
 # The syntax is:
-# pattern <= @(<variable-name>[,undef:<pattern>][,<def-name>=<pattern>]*)@
+# pattern <= @(<variable-name>[:undef=<pattern>][:<def-name>=<pattern>]*)@
 # variable-name <= pattern
 # def-name <= cmake-variable-name
 #
-# each of the def-name=<pattern> are used to set variables during the
+# each of the <def-name>=<pattern> are used to set variables during the
 # evaluation of this expression.
-#
-# undef:<pattern> Is the default value used if <variable-name> is not defined
+# As a special case unedf=<pattern> is used if <variable-name> is not defined
 #
 # As a shortcut the parenthesis in @()@ can be omitted if there are no nested
-# patterns. (e.g) @foo,undef:1@
+# patterns. (e.g) @foo:undef=1@
 #
 # The value of a variable @FOO@ is stored in ordinary (scoped)
 # cmake-variable AT_VAR_FOO, or if it is not defined FOO.
@@ -32,22 +31,22 @@
 # V1 = "value"                                "value"
 # V2 = "a ref to @V1@"                        "a ref to value"
 # V2a = "shortcut @V1@ or @(V1)@"             "shortcut value or value"
-# V3 = "A maybe ref @MAYBE,undef:foo@"        "A maybe ref foo"
-# V4 = "A recursive @(@OTHER,undef:V1@)@"     "A recursive value"
+# V3 = "A maybe ref @MAYBE:undef=foo@"        "A maybe ref foo"
+# V4 = "A recursive @(@OTHER:undef=V1@)@"     "A recursive value"
 # DOUBLE ="@arg@@arg@"
-# V5 = "Two @(@DOUBLE@,arg=bar)@"             "Two barbar"
+# V5 = "Two @(@DOUBLE@:arg=bar)@"             "Two barbar"
 # REF = "@(@ref@@index@)@"
-# V6 = "@V2@ is @REF,ref=V,index=2@"          "a ref to value is a ref to value"
+# V6 = "@V2@ is @REF:ref=V:index=2@"          "a ref to value is a ref to value"
 #
 
 # Expand variable expressions in input non-expressions are copied over
-# Expressions according to above: @(name[,undef:<str>][,name1=<value>])@
+# Expressions according to above: @(name[:undef=<str>][:name1=<value>])@
 # where:
 #    name - the name of a variable ([a-zA-Z_-]+) or a @-variable
 #           expression expanding to a name
-#    undef:<str> - uses <str> if no definition of name is found
+#    undef=<str> - uses <str> if no definition of name is found
 #    name1=<value> - define @name1@=<value> during expansion of name.
-# Where the undef:VALUE gets picked if we dont find name
+# Where the undef=VALUE gets picked if we dont find name
 # and foo=bar defines a (local) variable in the current expansion
 function(expand_variables input output_var)
   set(result "")
@@ -76,7 +75,10 @@ function(expand_variables input output_var)
       # message("Found @ at ${pos} expr = \"${var_expr}\" remainder: \"${input_remainder}\"")
       #Now var_expr is the content inside the expression separators
       expand_parsed_expression("${var_expr}" var_result)
+      # Let someone on the outside preview the expression expansion. This is
+      # used to capture expressions for the between-linker-passes-evaluation
       preview_var_replacement(value "${var_expr}" "${var_result}")
+
       string(APPEND result ${var_result})
       set(input "${input_remainder}")
     endif()
@@ -136,14 +138,14 @@ endfunction()
 function(parse_single_at_expression input var_expr remainder)
   string(FIND "${input}" "@" end_at)
   if(end_at EQUAL -1)
-    message(FATAL_ERROR "Syntax error in @ expression ${input}")
+    message(FATAL_ERROR "Syntax error in @ expression \"${input}\"")
   else()
     string(SUBSTRING "${input}" 0 ${end_at} var_str)
     math(EXPR remainder_start "${end_at} + 1")
     string(SUBSTRING "${input}" ${remainder_start} -1 input_remainder)
 
     #split the expression into a parts list.
-    string(REPLACE "," ";" var_str "${var_str}")
+    string(REPLACE ":" ";" var_str "${var_str}")
   endif()
   set(${var_expr} "${var_str}" PARENT_SCOPE)
   set(${remainder} "${input_remainder}" PARENT_SCOPE)
@@ -178,7 +180,7 @@ function(parse_paren_at_expression input var_expr remainder )
       math(EXPR marker_start_pos "${next_pos} - ${marker_length}")
       math(EXPR part_len "${marker_start_pos} - ${pos}")
       string(SUBSTRING "${input}" ${pos} ${part_len} part_str)
-      string(REPLACE "," ";" part_str "${part_str}")
+      string(REPLACE ":" ";" part_str "${part_str}")
       string(APPEND parts "${part_str}")
       if(marker STREQUAL ")@") # final closing
       else()
@@ -233,18 +235,20 @@ endfunction()
 # expression_parts - a list of expression parts (name,undef,defs)
 # result           - is set to the expanded value
 function(expand_parsed_expression expression_parts result)
+  set(original_parts "${expression_parts}")
   # Take the variable name from the list
   list(POP_FRONT expression_parts name)
 
-  # Find and handle undef:value and var=<value> parts
+  # Find and handle var=<value> parts, and undef
   set(undef_value)
   foreach( p IN LISTS expression_parts)
     # message("   Looking at part ${p}")
-    if(p MATCHES "^undef:(.*)")
-      set(undef_value ${CMAKE_MATCH_1})
-    elseif(p MATCHES "([^=]+)=(.*)")
+    if(p MATCHES "([^=]+)=(.*)")
       # Define a local variable, in this function scope.
       set(AT_VAR_${CMAKE_MATCH_1} "${CMAKE_MATCH_2}")
+      if(CMAKE_MATCH_1 STREQUAL "undef")
+        set(undef_value "${CMAKE_MATCH_2}")
+      endif()
     endif()
   endforeach()
 
@@ -272,7 +276,7 @@ function(expand_parsed_expression expression_parts result)
     expand_variables("${value}" new_value)
     if(new_value STREQUAL value)
       if(CMAKE_VERBOSE_MAKEFILE)
-        message("Using variable ${match} with value ${value}")
+        message("Expanded variable ${original_parts} with value ${value}")
       endif()
       set(${result} "${value}" PARENT_SCOPE)
       return()
@@ -287,7 +291,7 @@ endfunction()
 if(TEST_AT_VARIABLE_UTILS)
 
   function(preview_var_replacement result expression expanded)
-    message("  PREVIEW: ${expression} ${expanded}")
+    message("  PREVIEW: ${expression} => ${expanded}")
     set(${result} "${expanded}" PARENT_SCOPE)
   endfunction()
 
@@ -315,9 +319,9 @@ if(TEST_AT_VARIABLE_UTILS)
   test_parse_paren_at_expression("@(foo)@)@" "@(foo)@" "")
   test_parse_paren_at_expression("a@(b)@@(apa@(foo)@papa)@)@" "a@(b)@@(apa@(foo)@papa)@" "")
 
-  test_parse_paren_at_expression("@foo,undef:u@)@" "@foo,undef:u@" "")
-  test_parse_paren_at_expression("@(inner,undef:j)@,undef:u)@" "@(inner,undef:j)@;undef:u" "")
-  test_parse_paren_at_expression("@(inner,undef:j)@,undef:u)@,apa" "@(inner,undef:j)@;undef:u" ",apa")
+  test_parse_paren_at_expression("@foo:undef=u@)@" "@foo:undef=u@" "")
+  test_parse_paren_at_expression("@(inner:undef=j)@:undef=u)@" "@(inner:undef=j)@;undef=u" "")
+  test_parse_paren_at_expression("@(inner:undef=j)@:undef=u)@,apa" "@(inner:undef=j)@;undef=u" ",apa")
 
   function(test_vars expand expected)
     message("==========================================")
@@ -329,17 +333,26 @@ if(TEST_AT_VARIABLE_UTILS)
   set(AT_VAR_VAR1 "hello")
   set(AT_VAR_VAR2 "world")
   set(AT_VAR_REFER_TO_VAR2 "Something plus @VAR2@ is nice")
-  set(AT_VAR_WITH_PARAM "Some value @arg,undef:no_arg_given@")
-  set(AT_VAR_ARG1_OR_ARG2 "@(ARG1,undef:@ARG2@)@")
+  set(AT_VAR_WITH_PARAM "Some value @arg:undef=no_arg_given@")
+  set(AT_VAR_ARG1_OR_ARG2 "@(ARG1:undef=@ARG2@)@")
+
+  set(AT_VAR_MPU_ALIGN "MAX(0 , 2 << LOG2CEIL(@region_size@) )")
+  set(AT_VAR_SMEM_PARTITION_ALIGN "@MPU_ALIGN@")
+  set(AT_VAR_EVAL_MPU_ALIGN "@(Z_EVALUATE;undef=0;expr=MAX(32 , 2 << LOG2CEIL(@region_size@) ))@")
 
   test_vars("@VAR1@" "hello")
   test_vars("@VAR1@ @VAR2@" "hello world")
   test_vars("@VAR1@ @REFER_TO_VAR2@ @VAR2@" "hello Something plus world is nice world")
-  test_vars("@SOME_FUNKY,undef:undefined@" "undefined")
-  test_vars("@(SOME_FUNKY,undef:@VAR1@)@" "hello")
+  test_vars("@SOME_FUNKY:undef=undefined@" "undefined")
+  test_vars("@(SOME_FUNKY:undef=@VAR1@)@" "hello")
   test_vars("@WITH_PARAM@" "Some value no_arg_given")
-  test_vars("@WITH_PARAM,arg=something_given@" "Some value something_given")
-  test_vars("@(MY_OWN_DEF,MY_OWN_DEF=self)@" "self")
-  test_vars("@(ARG1_OR_ARG2,ARG2=self)@" "self")
-  test_vars("recursive @(@OTHER,undef:VAR1@)@" "recursive hello")
+  test_vars("@WITH_PARAM:arg=something_given@" "Some value something_given")
+  test_vars("@(MY_OWN_DEF:MY_OWN_DEF=self)@" "self")
+  test_vars("@(ARG1_OR_ARG2:ARG2=self)@" "self")
+  test_vars("recursive @(@OTHER:undef=VAR1@)@" "recursive hello")
+
+  test_vars("@(SMEM_PARTITION_ALIGN:undef=42:region_size=end-start)@" "MAX(0 , 2 << LOG2CEIL(end-start) )")
+
+  test_vars("@(EVAL_MPU_ALIGN:undef=4077:region_size=the_size)@" "0")
+  test_vars("@(EVAL_MPU_ALIGN:undef=4077:region_size=the_size:Z_EVALUATE=@(expr)@)@" "MAX(32 , 2 << LOG2CEIL(the_size) )")
 endif()
